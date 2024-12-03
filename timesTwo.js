b26804fdeafc682619b67b1c1e1812c1f9e76607
Redwingshunt <alohaass12@gmail.com>
2024-12-04 02:41:21 +0530

    initial push

---
+++ timesTwo.js
@@ -0,0 +1,11 @@
+// Task 1: Code the timesTwo function declaration
+// timesTwo.js
+
+function timesTwo(number) {
+    return number * 2;
+}
+
+
+
+// Task 2: Export the timesTwo function as a module
+module.exports = timesTwo;
---
+++ timesTwo.test.js
@@ -0,0 +1,8 @@
+
+// timesTwo.test.js
+const timesTwo = require('./timesTwo');
+
+test("returns the number times 2", () => {
+    expect(timesTwo(10)).toBe(20);
+});
+
